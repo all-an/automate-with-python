f892a408c48b32db05a66f3d7f4f0f8325458d6f
all-an <all-an@hotmail.com>
2023-09-19 19:27:16 -0300

    python basics

---
+++ 001-python-basics-and-first-automation/pybasics_two.py
@@ -0,0 +1,19 @@
+
+print(bool(""))
+
+print(bool(42))
+
+spam = 0
+
+while spam < 5:
+    print("hello spam")
+    spam = spam + 1
+
+name = ''
+
+while name != 'allan':
+    print('Please type your name: ')
+    name = input()
+
+print('Thank you!')
+    
