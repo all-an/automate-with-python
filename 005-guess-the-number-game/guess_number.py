5257d42ec223174446825beace4e00a1b155ba00
all-an <all-an@hotmail.com>
2023-09-25 20:35:28 -0300

    guess the number

--- 005-guess-the-number-game/guess_number.py
+++ 005-guess-the-number-game/guess_number.py
@@ -3,3 +3,26 @@
 import random
 
 print('Hello. What is your name?')
+name = input()
+
+print('Well, ' + name + ' I am thinking of a number between 1 and 20. You have 6 guesses')
+
+secretNumber = random.randint(1, 20)
+
+for guessesTaken in range(1, 7):
+    print('Take a guess: ')
+    try:
+        guessNumber = int(input())
+
+        if guessNumber < secretNumber:
+            print('Your guess is too low')
+        elif guessNumber > secretNumber:
+            print('Your guess is too high')
+        else:
+            print('Good job ! Congratulations')
+            break
+    except ValueError:
+        print('Invalid number, enter a digit')
+
+print('You took ' + str(guessesTaken) + ' guesses.')
+print('The number I was thinking of was ' + str(secretNumber))
