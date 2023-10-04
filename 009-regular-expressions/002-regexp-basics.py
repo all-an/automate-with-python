8fb4a20ee6c3ff4ef88161a853eb43743c738e64
all-an <all-an@hotmail.com>
2023-10-03 21:08:29 -0300

    regex

--- 009-regular-expressions/002-regexp-basics.py
+++ 009-regular-expressions/002-regexp-basics.py
@@ -11,3 +11,32 @@ print(matchObject.group())
 listMatch = phoneNumberRegex.findall(message)
 
 print(listMatch)
+
+
+#same thing
+phoneNumberRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d)')
+
+matchObject = phoneNumberRegex.search(message)
+
+print(matchObject.group())
+
+print(matchObject.group(1)) # output 345
+
+print(matchObject.group(2)) # output 342
+
+phoneNumberRegex = re.compile(r'\(\d\d\d\) (\d\d\d)-(\d\d\d\d)') # search litteraly for ( when \(
+
+message = 'Is this a phone? (345) 342-2341 and this 232-334-6666'
+
+matchObject = phoneNumberRegex.search(message)
+
+print(matchObject.group())
+print(matchObject.group(1)) # output 342
+
+batRegex = re.compile(r'Bat(man|mobile|copter|bat)')
+
+message = 'Superman vs Batman on the Batmobile'
+listMatch = batRegex.findall(message)
+
+print(listMatch)
+
