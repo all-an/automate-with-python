e65feadd8d19672c9b1dc637e39a54f181161938
all-an <all-an@hotmail.com>
2023-10-08 19:39:13 -0300

    verbose mode

---
+++ 009-regular-expressions/006-regex-sub-module-and-verbose-mode.py
@@ -0,0 +1,37 @@
+import re
+
+namesRegex = re.compile(r'Agent \w+')
+
+message = 'Agent Scully works with Agent Mulder'
+
+print(namesRegex.findall(message))
+#output ['Agent Scully', 'Agent Mulder']
+
+print(namesRegex.sub('Agent Smith', message)) #output Agent Smith works with Agent Smith
+
+print(message) # not modified
+
+namesRegex = re.compile(r'Agent (\w)\w*') # * means zero or more
+
+print(namesRegex.findall(message))
+#output ['S', 'M']
+
+print(namesRegex.sub(r'Agent \1****', message)) # \1 means group 1 (\w)
+#output Agent S**** works with Agent M****
+
+# verbose mode:
+
+verbose_regex = re.compile(r'''
+\d\d\d   # area code
+-        # first dash
+\d\d\d   # first three digits
+-        # second dash
+\d\d\d\d # last four digits                                              
+''', re.VERBOSE)
+
+message = """
+'Is this a phone? 345-342-2341 and this 232-334-6666'
+"""
+
+match = verbose_regex.findall(message)
+print(match)
