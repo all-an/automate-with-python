b5ac6e944044b3d3bb839f1b0d4828abfa4e3a8b
all-an <all-an@hotmail.com>
2023-10-13 20:16:35 -0300

    files

--- 010-files/001-file-names-and-paths.py
+++ 010-files/001-file-names-and-paths.py
@@ -27,7 +27,35 @@ print(os.path.abspath('spam.png')) # C:\spam.png
 
 os.chdir('C:\\Users\\Public\\code\\automate-with-python\\010-files')
 
-print(os.path.abspath('..\\..\\spam.png')) # C:\Users\Public\code\spam.png
+print(os.path.abspath('..\\..\\spam.png')) # C:\Users\Public\code\spam.png  # remove folders automate-with-python\\010-files
 
 print(os.path.isabs('..\\..\\spam.png')) # False
 print(os.path.isabs('C:\\Users\\Public\\code')) # True
+
+print(os.path.relpath('C:\\folder1\\folder2\\folder3\\spam.png', 'C:\\folder1')) # output folder2\folder3\spam.png
+
+print(os.path.dirname('C:\\folder1\\folder2\\folder3\\spam.png'))
+print(os.path.basename('C:\\folder1\\folder2\\folder3\\spam.png'))
+
+print(os.path.exists('C:\\Users\\Public\\code\\'))
+print(os.path.exists('C:\\folder1\\folder2\\folder3\\'))
+print(os.path.exists('C:\\windows\\system32\\calc.exe'))
+
+print(os.path.isdir('C:\\windows\\system32\\calc.exe'))
+print(os.path.isfile('C:\\windows\\system32\\calc.exe'))
+
+print(os.path.getsize('C:\\windows\\system32\\calc.exe'))
+
+print(os.listdir('C:\\Users\\Public\\code'))
+
+totalSize = 0
+for filename in os.listdir('C:\\windows\\system32\\'):
+    if not os.path.isfile(os.path.join('C:\\windows\\system32\\', filename)):
+        #print(os.path.isfile(os.path.join('C:\\windows\\system32\\', filename)))
+        continue
+    totalSize = totalSize + os.path.getsize(os.path.join('C:\\windows\\system32\\', filename))
+    #print(os.path.isfile(os.path.join('C:\\windows\\system32\\', filename)))
+
+print(totalSize)
+
+os.makedirs('C:\\Users\\Public\\code\\TEST1\\TEST2')
