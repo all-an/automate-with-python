392d9f3e8abb1da45370ba4f7b5be387d0fe476e
all-an <all-an@hotmail.com>
2023-10-21 17:59:57 -0300

    finishing os.walk

--- 010-files/005-walking_dir_files.py
+++ 010-files/005-walking_dir_files.py
@@ -1,4 +1,4 @@
-import os
+import os, shutil
 
 dir_to_walk = os.walk('C:\\Users\\Public\\code\\automate-with-python\\010-files\\dir_to_walk')
 
@@ -7,3 +7,12 @@ for folder_name, subfolders, filenames in dir_to_walk:
     print("SUBFOLDERS: " + str(subfolders))
     print("FILENAMES: " + str(filenames))
     print("---------------------------------------------------")
+    for sub_folder in subfolders:
+        if 'fish' in sub_folder:
+            shutil.rmtree(folder_name + '\\' + sub_folder)
+
+        for file in filenames:
+            if file.endswith('.py'):
+                shutil.copy(os.join(folder_name, file), os.join(folder_name, file + '.backup'))
+                # os.unlink(folder_name + '\\' + file)
+            
